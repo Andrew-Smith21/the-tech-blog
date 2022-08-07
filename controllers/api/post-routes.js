ee2871ede00b301d5232399b554876c081c5d954
Chad Smith <chad.smith@vanderbilt.edu>
2022-08-06 23:19:42 -0400

    update routes and models

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -10,6 +10,7 @@ router.get('/', (req, res) => {
     attributes: [
       'id',
       'post_url',
+      'post_text',
       'title',
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
@@ -44,6 +45,7 @@ router.get('/:id', (req, res) => {
     attributes: [
       'id',
       'post_url',
+      'post_text',
       'title',
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
