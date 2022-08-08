c5497ffe1e03e34f59554d802431b5e558c80749
Chad Smith <chad.smith@vanderbilt.edu>
2022-08-07 22:39:42 -0400

    updated dashboard

---
+++ public/javascript/add-post-button.js
@@ -0,0 +1,5 @@
+function addButtonHandler() {
+    document.querySelector('.create-form-container').classList.remove("display-none");
+}
+
+document.querySelector('.add-button').addEventListener('click', addButtonHandler);
